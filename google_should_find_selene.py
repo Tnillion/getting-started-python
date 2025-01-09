b91cb0c783209fb63e73c0c8ae2bb2a1d8d321c8
Artem Dvornikov <tnillion@gmail.com>
2025-01-09 21:56:13 +0300

    deleted url

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -10,5 +10,3 @@ def test_second(google_open):
     browser.element('[id="L2AGLb"]').click()
     browser.element('[name="q"]').should(be.blank).type('sewerser345345').press_enter()
     browser.element('[id="botstuff"]').should(have.text('По запросу sewerser345345 ничего не найдено.'))
-https://github.com/Tnillion/getting-started-python.git
-https://github.com/Tnillion/first_repo/getting-started-python.git/
